308ac2a27bf239ab4684e1bbf4ed7e84047a91c6
timot <timosl@protonmail.com>
2023-02-07 06:40:44 +0100

    modification

--- comet.py
+++ comet.py
@@ -8,11 +8,14 @@ class meteorite(pygame.sprite.Sprite):
         self.rect.x = randint(20,500)
         self.speed = randint(1,3)
         self.comet= fall
-
+    def delete(self):
+        self.comet.cometsgroup.remove(self)
     def falling(self):
         if self.rect.y< 600:
             self.rect.y += self.speed
         else:
-            self.comet.cometsgroup.remove(self)
-        if self.game.collision(self,self.game.players):
+            self.delete()
+        if self.comet.game.collision(self,self.comet.game.players):
+            self.delete()
+            self.comet.game.player.damage(5)
 
